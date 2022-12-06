4c11a1dbb79567d0804b2c103a8c085a9d6a1c9c
Suraj Kumar <znsuraj7@gmail.com>
2022-12-06 22:49:54 +0530

    all set

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/srijan", {
+    const conn = await mongoose.connect("mongodb+srv://znsuraj7:khukhundu@cluster0.idig2fu.mongodb.net/srijan?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
